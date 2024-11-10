44af020ce8ed1af7002a6f76897a3f4aeed49a47
Metatrooper1 <pritamadak453@gmail.com>
2024-11-10 19:16:47 +0530

    added test3 file to branch sos_n2

---
+++ src/test/Test3.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String Tired="no";
+		
+		if(Tired == "yes"||Tired=="y"|| Tired=="Y") {
+			System.out.println("Eat something");
+		}
+		else {
+			System.out.println("do work");
+		}
+
+	}
+
+}
